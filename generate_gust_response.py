0eb9df573a8b3576ac6948b8c55af493e99e98a3
sduess <s.dussler20@imperial.ac.uk>
2022-12-08 17:48:32 +0000

    refactor [case generator] clean up and update settings

--- generate_gust_response.py
+++ generate_gust_response.py
@@ -13,42 +13,39 @@ from pazy_wing_model import PazyWing
 
 import sharpy.utils.algebra as algebra
 
-def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_settings, u_inf = 1, rho = 1.225, surface_m = 4, gust_vanes = False, alpha = np.deg2rad(5.), symmetry_condition = False, delft_model = False):
+def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_settings, u_inf = 1, rho = 1.225, surface_m = 4, gust_vanes = False, alpha = np.deg2rad(5.), symmetry_condition = False, delft_model = False, test_case_settings = None):
      # simulation settings
     config = configobj.ConfigObj()
     config.filename = case_route + '/{}.sharpy'.format(case_name)
     settings = dict()
-    gravity = True #bool(not gust_vanes)
+    gravity = True
     n_step = 5
     structural_relaxation_factor = 0.6
     relaxation_factor = 0.2
     tolerance = 1e-6 #
-    fsi_tolerance = 1e-4 #8
+    fsi_tolerance = 1e-4 #
     num_cores = 4
-    variable_wake =  False #gust_vanes
+    variable_wake =  False 
 
-    u_inf = 18.
+    u_inf = 18.3
     rho = 1.205
-    # geometry parameters 
+
     chord = 0.1
-    # if gust_vanes:
-    #     chord = 0.3
-    #dt = 0.1/surface_m #2.5e-4
     CFL = 1
     dt = CFL * chord / surface_m / u_inf
-    # dtu_inf  = 2.5e-4
+    
     n_tstep = 10000
-    alpha = np.deg2rad(10.)
-    # # Test case 1
-    # alpha = np.deg2rad(5.)
+    alpha = np.deg2rad(5.)
     gust_frequency = 5.7 # Hz
     gust_T = 0.175439 #s 
     gust_amplitude = 0.805562
-    # Test case 2
-    # alpha = np.deg2rad(10.)
-    # frequency_gust = 3.2 # Hz
-    # gust_amplitude = 0.64458
-    # gust_T = 0.312500000000000
+
+    if test_case_settings is not None:
+        print("TEST CASE:", test_case_settings)
+        alpha = test_case_settings['alpha'] 
+        gust_T = test_case_settings['gust_T']
+        gust_frequency = test_case_settings['frequency_gust_vane']
+        gust_amplitude = test_case_settings['gust_amplitude']
 
     
 
@@ -66,7 +63,7 @@ def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_s
                  'Modal',
                 #  'BeamPlot',
                 #  'AerogridPlot',
-                #  'DynamicCoupled',
+                 'DynamicCoupled',
                 'SaveData'
                  ],
         'case': case_name, 'route': case_route,
@@ -111,7 +108,7 @@ def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_s
             'rho': rho, # Check why?? 1e-8,
             'print_info': True,
             'horseshoe': 'off',
-            'num_cores': 4,
+            'num_cores': num_cores,
             'n_rollup': 0, #settings['AerogridLoader']['mstar'],
             'rollup_dt': dt,
             'rollup_aic_refresh': 1,
@@ -122,6 +119,7 @@ def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_s
                 'u_inf_direction': [1.0, 0., 0.]},
             'vortex_radius': 1e-9,
             'symmetry_condition': symmetry_condition,
+            'symmetry_plane': 2
         }
     settings['StaticCoupled'] = {
         'print_info': 'on',
@@ -133,8 +131,8 @@ def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_s
         'aero_solver_settings':  {
             'rho': rho, # Check why?? 1e-8,
             'print_info': True,
-            'horseshoe': 'off',
-            'num_cores': 4,
+            'horseshoe': False,
+            'num_cores': num_cores,
             'n_rollup': 0, #settings['AerogridLoader']['mstar'],
             'rollup_dt': dt,
             'rollup_aic_refresh': 1,
@@ -145,10 +143,13 @@ def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_s
                 'u_inf_direction': [1.0, 0., 0.]},
             'vortex_radius': 1e-9,
             'symmetry_condition': symmetry_condition,
+            'symmetry_plane': 2
         },
         'structural_solver': 'NonLinearStatic',
         'structural_solver_settings': settings['NonLinearStatic']}
 
+    settings['LiftDistribution'] = {'coefficients': True,
+                                    'rho': 1.225}
     settings['BeamPlot'] = {}
 
     settings['AerogridPlot'] = {'include_rbm': 'off',
@@ -160,7 +161,7 @@ def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_s
 
 
     settings['Modal'] = {'NumLambda': 40,
-                        'rigid_body_modes': 'on',
+                        'rigid_body_modes': 'off',
                         'print_matrices': 'off',
                         'continuous_eigenvalues': 'off',
                         'write_modes_vtk': 'off',
@@ -174,36 +175,28 @@ def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_s
 
         
     settings['StepUvlm'] = {'num_cores': num_cores,
-                            'convection_scheme': 3,
+                            'convection_scheme': 2,
                             'gamma_dot_filtering': 7,
-                            'cfl1': variable_wake,
-                            'velocity_field_generator': 'SteadyVelocityField',
-                            'velocity_field_input': {'u_inf':u_inf,
-                                                    'u_inf_direction': [1., 0, 0]},
-                            # 'control_surface_deflection' : ['DynamicControlSurface'],
-                            # 'control_surface_deflection_generator_settings': {'0': {'dt': dt,'deflection_file': cs_deflection_file},},
+                            'cfl1': bool(not variable_wake),
+                            # 'velocity_field_generator': 'SteadyVelocityField',
+                            # 'velocity_field_input': {'u_inf':u_inf,
+                            #                         'u_inf_direction': [1., 0, 0]},
                             'velocity_field_generator': 'GustVelocityField',
                             'velocity_field_input':{'u_inf': u_inf,
                                                     'u_inf_direction': [1., 0, 0],
                                                     'relative_motion': True,
-                                                    'offset': 10 *dt * u_inf, #gust_settings['offset'],
-                            #                         # 'gust_shape': 'time varying',
-                            #                         # 'gust_parameters': {'file': '../02_gust_input/turbulence_time_600s_uinf_45_altitude_800_moderate_noise_seeds_10434_10435_10436_10437.txt',
-                            #                         #                     'yaw_angle': np.deg2rad(45),},
-                            #                         'gust_shape': '1-cos',
-                            #                         'gust_parameters': {'gust_length': gust_settings['length'],
-                            #                                             'gust_intensity': gust_settings['intensity']*u_inf,
-                            #                                             'yaw_angle': np.deg2rad(0.),
-                            #                                         }     
+                                                    'offset': 10 *dt * u_inf,    
                                                     'gust_shape': 'continuous_sin',
                                                     'gust_parameters': {'gust_length':  gust_T * u_inf, #gust_settings['length'],
                                                                         'gust_intensity': 2*gust_amplitude, # gust_settings['intensity']*u_inf,
+                                                                        'gust_component': 1
                                                                     }                                                                
                                                 },
                             'rho': rho,
                             'n_time_steps': n_tstep,
                             'dt': dt,
                             'symmetry_condition': symmetry_condition,
+                            'symmetry_plane': 2
                             }
 
     settings['NonLinearDynamicPrescribedStep'] = {'print_info': 'on',
@@ -224,15 +217,14 @@ def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_s
         'aero_solver_settings': settings['StepUvlm'],
         'fsi_substeps': 200,
         'fsi_tolerance': fsi_tolerance,
-        'relaxation_factor': relaxation_factor,
+        'relaxation_factor': 0., #relaxation_factor,
         'minimum_steps': 1,
         'relaxation_steps': 150,
-        'final_relaxation_factor': 0.05,
+        'final_relaxation_factor': 0.0,
         'n_time_steps': n_tstep,
         'print_info': True,
         'dt': dt,
         'include_unsteady_force_contribution': True, 
-    #   'postprocessors': ['BeamLoads','SaveData'],
         'postprocessors': ['BeamLoads', 'BeamPlot', 'AerogridPlot', 'SaveData'],
         'postprocessors_settings': {
                                     'BeamLoads': {'csv_output': 'off'},
@@ -246,32 +238,12 @@ def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_s
                                     'SaveData': settings['SaveData'],
                                     },
     }
-    print('Gust vanes = ', gust_vanes)
 
-    # if not gust_vanes:
-        
-        # settings['StepUvlm']['velocity_field_generator'] = 'SteadyVelocityField'
-        # settings['StepUvlm']['velocity_field_input'] = {'u_inf':u_inf,
-        #                                                 # 'relative_motion': True,
-        #                                                 'u_inf_direction': [1., 0, 0]}
-        # settings['StepUvlm']['convection_scheme'] = 2
-
-        # settings['AerogridLoader']['gust_vanes'] = False
-
-        # settings['DynamicCoupled']['controller_id'] = {'controller_tip': 'SurfaceOscillator'}
-        # settings['DynamicCoupled']['controller_settings'] = {'controller_tip': {'frequency': gust_settings['frequency'],
-        #                                                                         'amplitude': gust_settings['amplitude'],
-        #                                                                         'mean': gust_settings['mean'],                                                                                ''
-        #                                                                         'dt': dt,
-        #                                                                         'time_history_input_file': 'psi.csv',
-        #                                                                         'controller_log_route': './output/' + case_name + '/',
-        #                                                                         'controlled_surfaces': [0]}}
-    # else:
     if gust_vanes:
         gust_vane_parameters = {
             'M': 8,
             'N':20, #, 
-            'M_star': 20, 
+            'M_star': 100, 
             'span': 5, #10., 
             'chord': 0.3, 
             'control_surface_deflection_generator_settings': {
@@ -285,87 +257,67 @@ def set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_s
                                                                        'streamwise_position': [-1.5, -1.5],
                                                                        'vertical_position': [-0.25, 0.25],
                                                                        'symmetry_condition': symmetry_condition,
-                                                                       'vane_parameters': [gust_vane_parameters, gust_vane_parameters]
+                                                                       'vane_parameters': [gust_vane_parameters, gust_vane_parameters],
+                                                                       'vertical': True
                                                                       }
+    
         settings['StepUvlm']['convection_scheme'] = 3
-
-        #  For convergence study steady velocity field
         settings['StepUvlm']['velocity_field_generator'] = 'SteadyVelocityField'
         settings['StepUvlm']['velocity_field_input'] = {'u_inf':u_inf,
                                                         'u_inf_direction': [1., 0, 0]}
-                                                                       
-    # settings['StepUvlm']['velocity_field_generator'] = 'GustVelocityField',
-    # settings['StepUvlm']['velocity_field_input'] = {'u_inf': u_inf,
-    #                                             'u_inf_direction': [1., 0, 0],
-    #                                             'relative_motion': True,
-    #                                             'offset': gust_settings['offset'],
-    #                                             'gust_shape': 'continuous_sin',
-    #                                             'gust_parameters': {'gust_length':  gust_T * u_inf, #gust_settings['length'],
-    #                                                                 'gust_intensity': gust_amplitude, # gust_settings['intensity']*u_inf,
-    #                                                             }                                                                
-    #                                         },
+                                                                    
+
+
     for k, v in settings.items():
         config[k] = v
 
     config.write()
     return settings
 
-def setup_pazy_model(case_name, case_route, pazy_settings, gust_vanes = False, symmetry_condition = False):
+def setup_pazy_model(case_name, case_route, pazy_settings, gust_vanes = False, symmetry_condition = False, polars=None):
     pazy = PazyWing(case_name, case_route, pazy_settings)
     pazy.generate_structure()
     if not symmetry_condition:
-        print('mirror wing')
         pazy.structure.mirror_wing()
-    pazy.generate_aero()
+    if pazy_settings['model_id'] == 'delft':
+        pazy.structure.rotate_wing()
+    tip_load = 0# 0.285
+    if tip_load > 0.:
+        mid_chord_b = (pazy.get_ea_reference_line() - 0.5) * 0.1
+        pazy.structure.add_lumped_mass((tip_load, pazy.structure.n_node//2, np.zeros((3, 3)),
+                                np.array([0, mid_chord_b, 0])))
+        if not symmetry_condition:
+            pazy.structure.add_lumped_mass((tip_load, pazy.structure.n_node//2 + 1, np.zeros((3, 3)),
+                                            np.array([0, mid_chord_b, 0])))
 
+    pazy.generate_aero() 
     pazy.save_files()
     return pazy
 
-# def setup_gust_vanes(case_name, case_route = './cases/', output_route = './output/',
-#                      surface_m = 8, n_elem_multiplier = 1):
 
-#     # import gust_vane_model
-#     gust_vane_model = gust_vane_model.GustVane(case_name, case_route, output_route)
-#     gust_vane_model.clean()
-#     gust_vane_model.init_structure(sigma=1, n_elem_multiplier=n_elem_multiplier,
-#                                                chord = 0.3, span = 3.,
-#                                                streamwise_position = 0.,
-#                                                vertical_position = 0)
-
-#     gust_vane_model.init_aero(m=surface_m) 
-#     gust_vane_model.generate()   
-
-def run_test_gust_vane(case_name, test_case = '1', case_root='./cases/', output_folder='./output/'):
-    surface_m = 4
-    n_elem_multiplier = 2
-    # setup_gust_vanes(case_name, surface_m = surface_m, n_elem_multiplier=n_elem_multiplier)
-
-    gust_settings = {'amplitude': np.deg2rad(5.),
-                     'frequency': 5.7,
-                     'mean': 0.}
-    set_simulation_settings_dynamic(case_name, output_folder, case_root, gust_settings, surface_m, gust_vanes = True)
-
-    data = sharpy.sharpy_main.main(['', case_root + case_name + '.sharpy'])
-    return data
-def run_dynamic_prescriped_simulation_with_gust_input(skin_on, case_root='./cases/', output_folder='./output/', gust_vanes = False, symmetry_condition = False):
+def run_dynamic_prescriped_simulation_with_gust_input(skin_on, case_root='./cases/', output_folder='./output/', gust_vanes = False, symmetry_condition = False, test_case_settings = None):
     # pazy model settings
     # Norberto:  M = 16, N = 64
     pazy_model_settings = {'skin_on': skin_on,
                         'discretisation_method': 'michigan',
+                        'model_id': 'delft',
                         'num_elem': 2,
-                        'surface_m': 8}
-    case_name = 'pazy_nonsymmetry_unsteady_plus_gust_vanes_fine'
+                        'surface_m': 16,
+                        'symmetry_condition': symmetry_condition,
+                        }
+    case_name = 'pazy_vertical_case_1_gust_comp_1' #pazy_dynamic_alpha_587_gust_vane_test' #'pazy_modal_delft_aplha_12_symmetry_steady'
     case_route = case_root + '/' + case_name + '/'
 
     if not os.path.isdir(case_route):
         os.makedirs(case_route, exist_ok=True)
 
-    setup_pazy_model(case_name, case_route, pazy_model_settings, symmetry_condition = symmetry_condition)
+    setup_pazy_model(case_name, case_route, pazy_model_settings, symmetry_condition = symmetry_condition,polars=None) #generate_polar_arrays(airfoil_polar))
 
     gust_settings= {'length': 5,
                     'offset': 0,
-                    'intensity': 0.2,}
-    set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_settings, pazy_model_settings['surface_m'], gust_vanes = gust_vanes, symmetry_condition = symmetry_condition)
+                    'intensity': 0.2,
+                    }
+    set_simulation_settings_dynamic(case_name, output_folder, case_route, gust_settings, pazy_model_settings['surface_m'], gust_vanes = gust_vanes, symmetry_condition = symmetry_condition, delft_model = True, test_case_settings = test_case_settings)
 
     data = sharpy.sharpy_main.main(['', case_route + case_name + '.sharpy'])
     return data
@@ -382,16 +334,14 @@ if __name__ == '__main__':
                              'frequency_gust_vane': 3.2, #Hz
                              'gust_amplitude':0.3125}}
 
-    symmetry_condition = False #True
+    symmetry_condition = False # True
+    case = 1
     data = run_dynamic_prescriped_simulation_with_gust_input(skin_on='on',
                                                              case_root='./cases/',
                                                              output_folder='./output/',
-                                                             gust_vanes = True,
-                                                             symmetry_condition = symmetry_condition)
-    # run_test_gust_vane('gust_vane_test')
-    # setup_gust_vanes('gust_vane_test', 
-    #                  surface_m = 8,
-    #                  n_elem_multiplier=1)
-
-                                                              
+                                                             gust_vanes = False, #True,
+                                                             symmetry_condition = symmetry_condition,
+                                                             test_case_settings = dict_test_cases[str(case)]
+                                                             )
+                                             
 
